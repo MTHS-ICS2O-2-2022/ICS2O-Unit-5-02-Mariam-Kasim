a063d733c2710bdae8da6a716f3f3970886f0cd8
Mariam-Kasim <124389357+Mariam-Kasim@users.noreply.github.com>
2023-05-04 00:54:33 +0000

    main

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Mariam Kasim All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mariam Kasim
+// Created on: Apr 26
 // This file contains the JS functions for index.html
+
+"use strict"
+
+// Function to generate random potive and negative numbers
+function myButtonClicked() {
+  // Generate random number
+  let randomNumber = 0.00
+
+  // input
+  const positiveButtonChecked = document.getElementById('positive').checked
+
+  // process
+  if (positiveButtonChecked == true) {
+    // positive
+    randomNumber = Math.floor(Math.random() * 6)
+  } else {
+    // negative
+    randomNumber = Math.floor(Math.random() * -6)
+  }
+
+  // output
+  document.getElementById('answer').innerHTML = "The random number is: " + randomNumber
+}
